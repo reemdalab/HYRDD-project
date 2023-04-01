671434040560a83f6b447acdacc6ca62620d1570
reemdalab <78297370+reemdalab@users.noreply.github.com>
2023-04-02 02:12:53 +0300

    Update jobs.tsx

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -14,7 +14,7 @@ export default function Jobs()
     useEffect(() => setMounted(true), [])
     return(
      <main>
-        hhhhhhhhhh
+        tesst
         <Layout className={styles.layout}>
             <div style={{ visibility: !mounted ? 'hidden' : 'visible' }}>
                <Headers/>
